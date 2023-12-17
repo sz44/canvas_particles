ecf3d8fb8a3ab6ecb2be4c90a579f588a6532ff9
sz44 <semion44@gmail.com>
2023-12-17 11:53:12 -0800

    fixed trailOpacity var

--- particleEffects.js
+++ particleEffects.js
@@ -4,12 +4,15 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const particleArray = [];
+let hue = 0;
+let hueSpeed = 2;
+let trailOpacity = 0.10;
 
 class Particle {
   constructor() {
     this.x = mouse.x;
     this.y = mouse.y;
-    this.color = `hsl(120, 100%, 50%)`;
+    this.color = `hsl(${hue}, 100%, 50%)`;
     this.size = Math.random() * 20 + 2;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
@@ -54,15 +57,16 @@ function createParticles(n) {
 
 function animate() {
   // ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.fillStyle = "rgba(0,0,0,0.1)"
+  ctx.fillStyle = `rgba(0,0,0,${ trailOpacity })`
   ctx.fillRect(0,0,canvas.width,canvas.height);
   for (let j = 0; j<particleArray.length; j++) {
     particleArray[j].update();
     particleArray[j].draw();
-    if (particleArray[j].size < 0.3) {
+    if (particleArray[j].size < 0.2) {
         particleArray.splice(j, 1);
         j--;
     }
   }
+  hue += hueSpeed;
   window.requestAnimationFrame(animate);
 }
